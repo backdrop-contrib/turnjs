f530d86ebee226aa1051049d60f9da0fde392402
Kristian Polso <krisseck@gmail.com>
2012-09-29 16:29:33 +0300

    First working build

--- js/turnjs.js
+++ js/turnjs.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function(){
   var width = Drupal.settings.turnjs['page-width'];
   var height = Drupal.settings.turnjs['page-height'];
 
-  jQuery('#turnjs').turn({
+  jQuery('.drupal-turnjs').turn({
     'width': width,
     'height': height,
   });
