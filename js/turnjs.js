071a48c9c8f31c577058e01531e0803308358cac
Jen Lampton <jen@jeneration.com>
2021-11-01 21:30:47 -0700

    Add more settings, and forward/back keystrokes.

--- js/turnjs.js
+++ js/turnjs.js
@@ -5,12 +5,30 @@
     Drupal.behaviors.turnJS = {
       attach: function (context, settings) {
         $('.drupal-turnjs').turn({
+          acceleration: true,
+          gradients: true,
+          elevation: 50,
           width: settings.turnjs.pageWidth,
           height: settings.turnjs.pageHeight,
-          display: settings.turnjs.pageDisplay
+          display: settings.turnjs.pageDisplay,
+          when: {
+            turned: function(e, page) {
+              console.log('Current view: ', $(this).turn('view'));
+            }
+          }
         });
       }
     };
+
+    /* Enable page turning with left/right keys */
+    $(window).bind('keydown', function(e){
+      if (e.keyCode == 37) {
+        $('.drupal-turnjs').turn('previous');
+      }
+      else if (e.keyCode == 39) {
+        $('.drupal-turnjs').turn('next');
+      }
+    });
 
   }
 
