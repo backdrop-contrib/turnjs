74b888982f9afa75939ef2d87310f9287654f2fc
Jen Lampton <jen@jeneration.com>
2021-12-02 10:37:30 -0800

    Add page numbering, allow for page numbering offset.

--- js/turnjs.js
+++ js/turnjs.js
@@ -2,52 +2,60 @@
 
   if ($.isFunction( $.fn.turn )) {
 
+    // Check for a page number query perameter in the url.
+    var req_page = new URLSearchParams(location.search).get('page');
+    var calc_page = 1;
+    var offset = 0; // @todo UI for a page number offset.
+    if (req_page) {
+      calc_page = parseInt(req_page) + parseInt(offset);
+    }
+
     Drupal.behaviors.turnJS = {
       attach: function (context, settings) {
         $('.drupal-turnjs').turn({
+          page: calc_page,
           width: settings.turnjs.pageWidth,
           height: settings.turnjs.pageHeight,
           display: settings.turnjs.pageDisplay,
           acceleration: true,
-          elevation:50,
+          elevation: 50,
           when: {
+
             // Fired when the page is set to 1.
             first: function(e) {
-              //console.log('first - removing classes');
+              // Remove the even class for the first page.
               $(this).find('.p1').removeClass('even');
             },
+
             // Fired before a page starts turning.
             turning: function(e, page, view) {
               //console.log('turning - next page: ' + page);
+              //console.log('turning - view: ', view);
+            },
 
+            // Fired when a page has been turned.
+            turned: function(e, page, view) {
+
+              // Add even + odd classes to the pages.
               if (page != 1) {
-                //console.log('turning - adding classes');
-                var next_left = view[1] + 1;
-                var next_right = next_left + 1;
+                // Do NOT adjust by offset here.
+                var next_left = view[0];
+                var next_right = view[1];
                 $(this).find('.p'+next_left).addClass('odd').removeClass('even');
                 $(this).find('.p'+next_right).addClass('even').removeClass('odd');
               }
 
-            },
-            // Fired when a page has been turned.
-            turned: function(e, page, view) {
-              //console.log('turned - ', view);
+              // Add a query perameter to the URL for page number.
+              var left = page - offset; // Adjust by offset.
+              //console.log('turned - ', left);
+              var query = '?page=' + left;
+              window.history.replaceState(null, null, query);
+
             },
           },
         });
       }
     };
-
-    // Enable page turning with left/right keyboard keys.
-    $(window).bind('keydown', function(e){
-      if (e.keyCode == 37) {
-        $('.drupal-turnjs').turn('previous');
-      }
-      else if (e.keyCode == 39) {
-        $('.drupal-turnjs').turn('next');
-      }
-    });
-
   }
 
 })(jQuery);
